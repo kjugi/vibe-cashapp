af0d20093ccccaec87cca1cc0969bc0dba10c2c7
Filip Kudla <11212541+kjugi@users.noreply.github.com>
2026-09-15 00:25:21 +0200

    add github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const base = process.env.GITHUB_ACTIONS ? '/vibe-cashapp/' : '/'
+
 export default defineConfig({
+  base,
   plugins: [
     react(),
     VitePWA({
@@ -16,8 +19,8 @@ export default defineConfig({
         background_color: '#f3eee4',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
-        scope: '/',
+        start_url: base,
+        scope: base,
         icons: [
           {
             src: 'favicon.svg',
